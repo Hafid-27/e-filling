33d5cc6d42cafd8d6c7825e1f8b269cb53d42e88
Haykal Rizki <haykalrizki59093@gmail.com>
2022-10-19 18:31:01 +0700

    Rename folder views-backend-admin system

--- application/views/backend/admin_system/peminjam.php
+++ application/views/backend/admin_system/peminjam.php
@@ -1,8 +1,8 @@
             <div class="row">
-                <div class="col-lg-12">
-                    <h1 class="page-header">Data Peminjam</h1>
-                </div>
-                <!-- /.col-lg-12 -->
+              <div class="col-lg-12">
+                <h1 class="page-header">Data Peminjam</h1>
+              </div>
+              <!-- /.col-lg-12 -->
             </div>
             <!-- /.row -->
             <div class="row">
@@ -10,7 +10,7 @@
                 <div class="panel panel-default">
                   <div class="panel-heading">
                     <div class="text-right">
-                      <div class="pull-left panel-title">Data User Peminjam</div>
+                      <div class="pull-left panel-title">List User Peminjam</div>
                       <a class="btn btn-success btn-add" href="<?php echo base_url('admin_system/peminjam_form') ?>"><i class="fa fa-plus"></i> Tambah Data</a>
                     </div>
                   </div>
@@ -22,7 +22,7 @@
                           <th>ID</th>
                           <th>Username</th>
                           <th>Password</th>
-                          <th>Nama</th>
+                          <th>Nama Karyawan</th>
                           <th>Email</th>
                           <th>Edit</th>
                           <th>Hapus</th>
@@ -30,25 +30,25 @@
                       </thead>
                       <tbody>
                         <?php foreach ($data as $d1) { ?>
-                        <tr>
-                          <td><?php echo $d1->id_peminjam ?></td>
-                          <td><?php echo $d1->username ?></td>
-                          <td><?php echo $this->encrypt->decode($d1->password)?></td>
-                          <td><?php echo $d1->name ?></td>
-                          <td><?php echo $d1->email ?></td>
-                          <td class="text-center">
-                            <form action="<?php echo base_url('admin_system/peminjam_form') ?>" method="post">
-										          <input type="hidden" name="id" value="<?php echo $d1->id_peminjam ?>">
-										          <button class="btn btn-info btn-xs btn-edit" type="submit" data-original-title="Ubah" data-placement="top" data-toggle="tooltip"><i class="fa fa-edit"></i></button>
-								 	          </form>
-                          </td>
-                          <td class="text-center">
-                            <form action="<?php echo base_url('admin_system/peminjam_delete') ?>" method="post">
-										          <input type="hidden" name="id" value="<?php echo $d1->id_peminjam ?>">
-										          <button class="btn btn-danger btn-xs btn-delete" type="submit" data-original-title="delete" data-placement="top" data-toggle="tooltip"><i class="fa fa-trash-o"></i></button>
-								 	          </form>
-                          </td>
-                        </tr>
+                          <tr>
+                            <td><?php echo $d1->id_peminjam ?></td>
+                            <td><?php echo $d1->username ?></td>
+                            <td><?php echo $this->encrypt->decode($d1->password) ?></td>
+                            <td><?php echo $d1->name ?></td>
+                            <td><?php echo $d1->email ?></td>
+                            <td class="text-center">
+                              <form action="<?php echo base_url('admin_system/peminjam_form') ?>" method="post">
+                                <input type="hidden" name="id" value="<?php echo $d1->id_peminjam ?>">
+                                <button class="btn btn-info btn-xs btn-edit" type="submit" data-original-title="Ubah" data-placement="top" data-toggle="tooltip"><i class="fa fa-edit"></i></button>
+                              </form>
+                            </td>
+                            <td class="text-center">
+                              <form action="<?php echo base_url('admin_system/peminjam_delete') ?>" method="post">
+                                <input type="hidden" name="id" value="<?php echo $d1->id_peminjam ?>">
+                                <button class="btn btn-danger btn-xs btn-delete" type="submit" data-original-title="delete" data-placement="top" data-toggle="tooltip"><i class="fa fa-trash-o"></i></button>
+                              </form>
+                            </td>
+                          </tr>
                         <?php } ?>
                       </tbody>
                     </table>
